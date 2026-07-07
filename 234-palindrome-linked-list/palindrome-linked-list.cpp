50ca9ffab6152869dcb3be57bf88e393ea71988a
Bhaumik Jangid <148542505+bhaumik-jangid@users.noreply.github.com>
2026-07-07 11:49:51 +0530

    Time: 3 ms (59.65%) | Memory: 118.1 MB (53.35%) - LeetSync

--- 234-palindrome-linked-list/palindrome-linked-list.cpp
+++ 234-palindrome-linked-list/palindrome-linked-list.cpp
@@ -1,31 +1,56 @@
-/**
- * Definition for singly-linked list.
- * struct ListNode {
- *     int val;
- *     ListNode *next;
- *     ListNode() : val(0), next(nullptr) {}
- *     ListNode(int x) : val(x), next(nullptr) {}
- *     ListNode(int x, ListNode *next) : val(x), next(next) {}
- * };
- */
 class Solution {
 public:
+
+    ListNode* reverseL(ListNode* head) {
+        if(head == NULL) return head;
+        ListNode *prev = NULL, *curr = head, *nex = NULL;
+
+        while(curr != NULL){
+            nex = curr->next;
+            curr->next = prev;
+            prev = curr;
+            curr = nex;
+        }        
+        head = prev;
+        return head;
+    }
+
     bool isPalindrome(ListNode* head) {
-        stack<int> s;
+        if(head == NULL || head->next == NULL) return head;
 
-        ListNode* temp = head;
-        while(temp != NULL) {
-            s.push(temp->val);
-            temp = temp->next;
+        ListNode *slow = head, *fast = head;
+
+        while(fast != NULL && fast->next != NULL) {
+            slow = slow->next;
+            fast = fast->next->next;
         }
 
-        temp = head;
-        while(temp != NULL) {
-            if(s.top() != temp->val) return false;
-            s.pop();
-            temp = temp->next;
+        ListNode *head2 = reverseL(slow);
+
+        while(head2 != NULL) {
+            if(head->val != head2->val) return false;
+            head = head->next;
+            head2 = head2->next;
         }
 
-        return true;;
+        return true;
+
+
+        // stack<int> s;
+
+        // ListNode* temp = head;
+        // while(temp != NULL) {
+        //     s.push(temp->val);
+        //     temp = temp->next;
+        // }
+
+        // temp = head;
+        // while(temp != NULL) {
+        //     if(s.top() != temp->val) return false;
+        //     s.pop();
+        //     temp = temp->next;
+        // }
+
+        // return true;;
     }
 };
